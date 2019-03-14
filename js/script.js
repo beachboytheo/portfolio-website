3801a81a11952ada480316d5fa5667fd043ca606
sjtheo <beachboytheo@yahoo.com>
2019-03-14 17:57:13 -0400

    Code quality and cross browser testing

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ console.log("Hi! Welcome to my portfolio site")
 
 $(document).ready(function() {
   $(".project-preview").on("click", function() {
-    $(this).siblings(".project-details").slideToggle();
+    $(this).siblin gs(".project-details").slideToggle();
     });
 });
